4bcd5a37674f129fe611936c1ea734a0ad56805b
Ramazan Güner <ramazan.guner@turkcell.com.tr>
2021-01-27 14:03:53 +0300

    git ignore

---
+++ redishash/src/main/java/com/denemeler/redishash/RedishashApplication.java
@@ -0,0 +1,14 @@
+package com.denemeler.redishash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedishashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedishashApplication.class, args);
+		
+	}
+
+}
---
+++ redishash/src/test/java/com/denemeler/redishash/RedishashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denemeler.redishash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedishashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
